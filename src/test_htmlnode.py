126cecb32ebd152b0dba2cc7e7b43baf4116e3ce
HenningRixen <henning.rixen@freenet.de>
2024-03-06 19:58:37 +0100

    htmlnode plus test

---
+++ src/test_htmlnode.py
@@ -0,0 +1,18 @@
+import unittest
+
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        node = HTMLNode(
+        "h1", 
+        "hi there", 
+        None, 
+        {"href": "https://www.google.com", "target": "_blank"},
+        )
+        self.assertEqual(node.props_to_html(),  ' href="https://www.google.com" target="_blank"'
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
