5f286c625170204547214c1cfc4d3f8c4db09289
HenningRixen <henning.rixen@freenet.de>
2024-03-06 19:19:38 +0100

    test node expanded

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -8,6 +8,32 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", "bold")
         node2 = TextNode("This is a text node", "bold")
         self.assertEqual(node, node2)
+    
+    def test_not_text_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a tex node", "bold")
+        self.assertEqual(node, node2)
+    
+    def test_not_text_type_eq(self):
+        node = TextNode("This is a text node", "bold")
+        node2 = TextNode("This is a tex node", "italic")
+        self.assertEqual(node, node2)
+    
+    def test_url_eq(self):
+        node = TextNode("This is a text node", "bold", "www.google.com")
+        node2 = TextNode("This is a text node", "bold", "www.google.com")
+        self.assertEqual(node, node2)
+    
+    def test_not_url_eq(self):
+        node = TextNode("This is a text node", "bold", "www.google.com")
+        node2 = TextNode("This is a text node", "bold", "www.foogle.com")
+        self.assertEqual(node, node2)
+    
+    def test_repr(self):
+        node = TextNode("This is a text node", bold, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, bold, https://www.boot.dev)", repr(node)
+        )
 
 
 if __name__ == "__main__":
