6fa7dda76b2f5afc5091d733c138b7b1d3b674ab
HenningRixen <henning.rixen@freenet.de>
2024-03-08 15:30:11 +0100

    currently working textnode and test_textnode, reminder

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,15 @@
 import unittest
 
-from textnode import TextNode
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_image,
+    text_type_link,
+)
+
 
 
 class TestTextNode(unittest.TestCase):
